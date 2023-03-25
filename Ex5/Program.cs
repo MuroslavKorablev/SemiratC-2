cec8ce2b9c70da4af8959b294f09bd073e9eb108
Kleo <mmttdd16@gmail.com>
2023-03-25 12:12:22 +0300

    Working Ex5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next (1,100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[30];
+
+FillArray(array);
+PrintArray(array);
