b8bf023163771734899afae32690036704dcd4be
Kleo <mmttdd16@gmail.com>
2023-03-25 11:43:06 +0300

    Create Ex5

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {22,1,2,4,14,12,5,6,7,12,41};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while( index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine("found number index = " + index);
+        break;
+    }
+    index++;
+}
